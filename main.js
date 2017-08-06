32d0fd659ecc063d48868b444826cc5f14e946c1
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-08-06 18:14:04 -0400

    tryig to figure out how to make onclick work

---
+++ main.js
@@ -0,0 +1,17 @@
+let displayVal = document.querySelector('body').querySelector(".wrapper").querySelector(".calculator").querySelector(".row-1").querySelector("#display").value;
+
+function calcAdd () {
+
+}
+
+function calcSub (){
+
+}
+
+function calcMult (){
+
+}
+
+function calcDiv (){
+
+}
