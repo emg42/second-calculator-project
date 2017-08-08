6dfced216cff930e00d00f7e47440cb00601e3da
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-08-08 18:00:00 -0400

    was going to do an if statement that includes clears and equals but got too complicated. Might be changing gears towards separating out clear and equals

--- main.js
+++ main.js
@@ -3,29 +3,25 @@ const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
 console.log(buttons);
 let input ="";
-function calcAdd () {
 
-}
-
-function calcSub (){
-
-}
-
-function calcMult (){
-
-}
-
-function calcDiv (){
-
-}
-
-function disValue () {
-
-}
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', userInput)
 }
 
 function userInput () {
+if (this.value == 'c'){
+  clrScreen();
+} else if(this.value == '='){
 
+}
+}
+
+
+function showInput(string){
+  display.textContent
+}
+
+function clrScreen() {
+input="";
+showInput(input);
 }
