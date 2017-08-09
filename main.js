3f9e2b1b5fe4ec28525ab325dfb97354604bde40
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-08-09 16:53:10 -0400

    more changes

--- main.js
+++ main.js
@@ -4,18 +4,15 @@ const equalBtn = document.querySelector('#equals');
 const calcBtns = document.querySelectorAll('.calc-button');
 const display = document.querySelector('#display');
 
-let displayInput ="";
+let input ="";
 
-// function addToDisplay (){
-//
-//
-// }
+let valuesTogether ="";
 
-calcBtns.addEventListener('click', function(){
-  let displayValues ="";
-  for (let i = 0; i < calcBtns.length; i++) {
-  displayValues.push(calcBtns[i].textContent);
-  }
-  console.log(displayValues);
 
-})
+
+
+function displayInput (){
+let value = this.textContent;
+valuesTogether += value;
+}
+calcBtns.addEventListener('click', displayInput);
