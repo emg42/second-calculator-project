a6fbb5c159f9176a1eb29acd2d38630184312ef5
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-08-09 16:21:08 -0400

    added ids to each number and starting on declaring some variables in the js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const clrBtn = document.querySelector('#clears');
+const equalBtn = document.querySelector('#equals');
 
+const calcBtns = doucment.querySelectorAll('.calc-button');
+
+for (let i = 0; i < calcBtns.length; i++) {
+  calcBtns[i]
+}
