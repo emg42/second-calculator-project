113bfadfaa5d9fbc79bdc7ee0cdb13f9d457a2fa
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-08-09 16:41:19 -0400

    created some stuff and commented some out

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const clrBtn = document.querySelector('#clears');
 const equalBtn = document.querySelector('#equals');
 
-const calcBtns = doucment.querySelectorAll('.calc-button');
+const calcBtns = document.querySelectorAll('.calc-button');
+const display = document.querySelector('#display');
 
-for (let i = 0; i < calcBtns.length; i++) {
-  calcBtns[i]
-}
+let displayInput ="";
+
+// function addToDisplay (){
+//
+//
+// }
+
+calcBtns.addEventListener('click', function(){
+  let displayValues ="";
+  for (let i = 0; i < calcBtns.length; i++) {
+  displayValues.push(calcBtns[i].textContent);
+  }
+  console.log(displayValues);
+
+})
