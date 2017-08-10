1686ea1ec3e9f3267b7e01607893c1dddf4aea37
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-08-10 08:56:56 -0400

    added more functions for each button

--- main.js
+++ main.js
@@ -34,8 +34,8 @@ let values ="";
 // num5.addEventListener('click', calcValues);
 // num6.addEventListener('click', calcValues);
 num1.addEventListener('click', numOne);
-// num2.addEventListener('click', calcValues);
-// num3.addEventListener('click', calcValues);
+num2.addEventListener('click', numTwo);
+num3.addEventListener('click', numThree);
 // num0.addEventListener('click', calcValues);
 
 
@@ -44,19 +44,57 @@ function numOne (){
   values += "1";
   display.textContent =  values;
 }
-function numTwo
-// function numThree
-// function numFour
-// function numFive
-// function numSix
-// function numSeven
-// function numEight
-// function numNine
-// function numPlus
-// function numClr
+function numTwo() {
+  values += "2";
+  display.textContent =  values;
+}
+function numThree(){
+  values += "3";
+  display.textContent =  values;
+}
+function numFour() {
+  values += "4";
+  display.textContent =  values;
+}
+function numFive() {
+  values += "5";
+  display.textContent =  values;
+}
+function numSix() {
+  values += "6";
+  display.textContent =  values;
+}
+function numSeven() {
+  values += "7";
+  display.textContent =  values;
+}
+function numEight() {
+  values += "8";
+  display.textContent =  values;
+}
+function numNine() {
+  values += "9";
+  display.textContent =  values;
+}
+function numPlus() {
+  values += "+";
+  display.textContent =  values;
+}
+function numMinus() {
+  values += "+";
+  display.textContent =  values;
+}
+function numPlus() {
+  values += "-";
+  display.textContent =  values;
+}
+
+// function numClr() {
+//   values += "";
+//   display.textContent =  values;
+// }
 // function numEquals
-// function numMinus
-// function numPlus
+
 
 
 // function displayInput (){
